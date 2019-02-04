5c162075b6a4398aab5a67ac6116a9ecdfff9a66
Sassan Kermani <sassankermani@Sassans-iMac.local>
2019-02-04 07:16:51 -0700

    ok

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,11 +1,68 @@
-/*=============================================
-=            Section comment block            =
-=============================================*/
+/*=============================
+=            SetUp            =
+=============================*/
 
 let fs = require(`fs`);
 const bcrypt = require(`bcrypt`);
 
-/*=====  End of Section comment block  ======*/
+let dayPassLength = 100;
+
+/*=====  End of SetUp  ======*/
+
+/*==================================
+=            Genral use            =
+==================================*/
+
+let hashPassword = (password)=>{
+
+	let tempPromis = new Promise((resolve, reject)=>{
+
+		bcrypt.genSalt( (err, salt)=>{
+			if(err){
+				console.log(`loginController.js hashPassword err`);
+			}
+			else{
+				bcrypt.hash(`${password}`, salt, (errr, hash)=>{
+					if(errr){
+						console.log(`loginController.js hashPassword err`);
+					}
+					else{
+						resolve(hash);
+					}
+				})
+			}
+		})
+	});
+
+	return tempPromis;
+}
+
+let checkPassword = (passwordFromDb, passwordFromUser)=>{
+
+	let tempPromis = new Promise((resolve, reject)=>{
+
+		bcrypt.compare( `${passwordFromDb}`, `${passwordFromUser}`, (err, passwordMatch)=>{
+			resolve(passwordMatch);
+		});
+
+	});
+
+	return(tempPromis);
+
+}
+
+let randomeDayPass = ()=>{
+	let options = `abcdefghijklmnopqrstuvABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{},./<>?;:'"`;
+	
+	let randStr = ``;
+
+	for(let i = 0; i < dayPassLength; i++){
+		randStr = randStr.concat();
+		// `${options.charAt(  Math.floor(Math.random() * Math.floor(  options.length  )  )   }`
+	}
+}
+
+/*=====  End of Genral use  ======*/
 
 
 /*----------  exports  ----------*/
